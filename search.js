7b39df668b0716f14db98852a034670e11053cd8
sabriAlshibani <sbryalshybany1@gmail.com>
2024-03-18 15:08:27 +0300

    done some tests in js

--- search.js
+++ search.js
@@ -1,10 +1,36 @@
-function search () {
-    let keyWord = document.querySelector("searching").value;
+function search() {
+    let x = document.getElementById("searching");
+    let y = x.value;
     let words = ["car", "bus", "sun", "moon"];
-    if (words.includes(keyWord)) {
-        document.write(words);
+    if (words.includes(y)) {
+        document.querySelector("#put").innerHTML = words;
     }
     else {
-        document.write("not found")
+        document.querySelector("#put").innerHTML = "not found";
+
     }
 }
+
+
+
+
+function sayHello(userName) {
+    console.log(userName);
+}
+sayHello("hello");
+let x = document.getElementById("change"); // take the element and make it on the variable 
+x.querySelector("p").innerHTML = "hossam" // take the element then change it 
+
+function background() {
+    let y = document.getElementById("test");
+    y.querySelector("#color").innerHTML = "you can enter";
+    y.querySelector("#color").style.backgroundColor = "red"; // change the color 
+    document.getElementById("button").style.backgroundColor = "red"
+}
+function box() {
+    document.getElementById("box").style.backgroundColor = "red";
+    document.getElementById("box").style.width = "fit-content";
+
+let div = document.getElementById("box");
+div.querySelector("#new").innerHTML = "hello guys";
+} 
